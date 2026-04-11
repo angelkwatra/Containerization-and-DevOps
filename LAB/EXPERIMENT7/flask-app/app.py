069f0e14f2457b5d8bdbc035d30689500a4f0e83
angelkwatra <angelkwatra123@gmail.com>
2026-04-11 09:45:46 +0530

    Testing GitHub Webhook

--- LAB/EXPERIMENT7/flask-app/app.py
+++ LAB/EXPERIMENT7/flask-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello CI/CD! This is my automated pipeline!"
+    return "now i am testing it."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
